331a4738ac2019c113cf519d2ede5bedd1fefe06
Shriya Sorte <shriyasorte2000@gmail.com>
2024-06-30 22:29:49 +0530

    changes in user controller

--- controller/userController.js
+++ controller/userController.js
@@ -20,11 +20,11 @@ async function loginuser(req,res){
         if(!user || !(await user.comparepassword(password))){
             return res.status(400).send({message: 'User not found'});
         }
-        const token = jwt.sign({_id:user._id}, "secretkey", {expiresIn: '1h'});
+        const token = jwt.sign({_id:user._id}, 'shriya', {expiresIn: '1h'});
         res.status(200).send({user, token});
     } catch (error) {
         res.status(400).send(error);
     }
-}
+};
 
 module.exports = {registeruser, loginuser};
