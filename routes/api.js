9b3fc02a4574a5b1bc98fc87a88ebf88484bfbd4
Shriya Sorte <shriyasorte2000@gmail.com>
2024-07-03 14:06:15 +0530

    code update

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 const userController = require('../controller/userController');
+const eventController = require('../controller/eventController');
 const express = require('express');
 const router = express.Router();
 
 // user routes
 router.post('/registeruser', userController.registeruser);
 router.post('/loginuser', userController.loginuser);
+
+// event routes
+router.get('/getAllEvents', eventController.getAllEvents);
+router.get('/getSingleEvent', eventController.getSingleEvent);
+router.post('/addEvent', eventController.addEvent);
+router.put('/updateEvent/:id', eventController.updateEvent);
+router.delete('/deleteEvent/id', eventController.deleteEvent);
 
 module.exports = router;
